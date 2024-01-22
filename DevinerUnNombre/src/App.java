d0f57a6eb1e3a195e7be42282cae5b3d5f8ee185
Leo_m <l.marque69210@gmail.com>
2024-01-22 12:02:23 +0100

    juste prix

--- DevinerUnNombre/src/App.java
+++ DevinerUnNombre/src/App.java
@@ -1,5 +1,30 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int nbADeviner = 0;
+        int nbSaisi = 0;
+        int nbEssais = 0;
+        // objet qui sera charger de choisir un nombre compris entre 1 et 100
+        Random random = new Random();
+
+        nbADeviner = 1 + random.nextInt(100);
+        // System.out.println(nbADeviner);
+
+        System.out.println("Entrez un nombre");
+        while (nbADeviner != nbSaisi) {
+            
+            nbSaisi = scanner.nextInt();
+            if (nbADeviner > nbSaisi){
+                System.out.println("C'est plus");
+            }else if (nbADeviner<nbSaisi){
+                System.out.println("c'est moins");
+            }else{
+                System.out.println("C'est juste");
+                System.out.println("Vous avez réussi en " + nbEssais + " essais");
+            }
+            nbEssais = nbEssais + 1;
+        }
+        scanner.close();
     }
 }
