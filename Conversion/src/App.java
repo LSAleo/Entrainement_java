b8a2f17a3442f3ba82a23fd7b6cfed2ad9b0083d
Leo_m <l.marque69210@gmail.com>
2024-01-22 11:24:55 +0100

    conversion km miles

--- Conversion/src/App.java
+++ Conversion/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // on crée un objet de type scanner
+        Scanner scanner = new Scanner(System.in);
+        float distanceEnKm = 0;
+        float distanceEnMiles = 0;
+        float tauxDeConversion = 1.6f;
+
+        // Programme qui demande une distance en km et qui la convertit en miles
+        System.out.println("Entrez une distance en km");
+        distanceEnKm = Float.parseFloat(scanner.nextLine());
+        scanner.close();
+        System.out.println("Vous avez saisi" + distanceEnKm);
+        distanceEnMiles = distanceEnKm / tauxDeConversion;
+        System.out.println("La distance en miles est :" + distanceEnMiles);
     }
 }
