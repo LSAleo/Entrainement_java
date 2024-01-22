4a2ec841582d77e4240ce310d307f4c82a37dc86
Leo_m <l.marque69210@gmail.com>
2024-01-22 10:41:15 +0100

    commit des premiers exercices

---
+++ Hello world/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    // Méthode éxécutable
+    public static void main(String[] args){
+        // System est une class
+        // out correspond à l"'écran
+        // println est une méthode
+        System.out.println("Hello World");
+    }
+}
