efc3a4a4b4f73b9baa5635eed107598f661577ca
Leo_m <l.marque69210@gmail.com>
2024-01-23 16:25:47 +0100

    début motus

--- Motus/src/fr/it_akademy/motus/leo/business/Essai.java
+++ Motus/src/fr/it_akademy/motus/leo/business/Essai.java
@@ -1,5 +1,57 @@
 package fr.it_akademy.motus.leo.business;
+import java.time.LocalDateTime;
 
 public class Essai {
+    private Long id;
+    private String contenu;
+    private LocalDateTime dateHeureEnvoi;
+    private static Long compteur = 0L;
+
+    public Essai() {
+        id = ++compteur;
+    }
+
+    public Essai(String contenu, LocalDateTime dateHeureEnvoi) {
+        this();
+        this.contenu = contenu;
+        this.dateHeureEnvoi = dateHeureEnvoi;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+
+    public LocalDateTime getDateHeureEnvoi() {
+        return dateHeureEnvoi;
+    }
+
+    public void setDateHeureEnvoi(LocalDateTime dateHeureEnvoi) {
+        this.dateHeureEnvoi = dateHeureEnvoi;
+    }
+
+    public static Long getCompteur() {
+        return compteur;
+    }
+
+    public static void setCompteur(Long compteur) {
+        Essai.compteur = compteur;
+    }
+
+    @Override
+    public String toString() {
+        return "Essai [id=" + id + ", contenu=" + contenu + ", dateHeureEnvoi=" + dateHeureEnvoi + "]";
+    }
     
 }
--- Motus/src/fr/it_akademy/motus/leo/business/Partie.java
+++ Motus/src/fr/it_akademy/motus/leo/business/Partie.java
@@ -1,5 +1,46 @@
 package fr.it_akademy.motus.leo.business;
+import java.time.LocalDateTime;
 
 public class Partie {
-    
+    private Long id;
+    private LocalDateTime dateHeureDebut;
+    private static Long compteur = 0L;
+
+    public Partie() {
+        id = ++compteur;
+    }
+
+    public Partie(LocalDateTime dateHeureDebut) {
+        this();
+        this.dateHeureDebut = dateHeureDebut;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateHeureDebut() {
+        return dateHeureDebut;
+    }
+
+    public void setDateHeureDebut(LocalDateTime dateHeureDebut) {
+        this.dateHeureDebut = dateHeureDebut;
+    }
+
+    public static Long getCompteur() {
+        return compteur;
+    }
+
+    public static void setCompteur(Long compteur) {
+        Partie.compteur = compteur;
+    }
+
+    @Override
+    public String toString() {
+        return "Partie [id=" + id + ", dateHeureDebut=" + dateHeureDebut + "]";
+    }
 }
