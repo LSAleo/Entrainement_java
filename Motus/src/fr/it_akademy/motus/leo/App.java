b5e3005ad0f0a7442376d3c380edafb8b2eb938e
Leo_m <l.marque69210@gmail.com>
2024-01-24 13:43:42 +0100

    on commence app.java du motus

--- Motus/src/fr/it_akademy/motus/leo/App.java
+++ Motus/src/fr/it_akademy/motus/leo/App.java
@@ -1,9 +1,27 @@
 package fr.it_akademy.motus.leo;
-import business.*;
+import fr.it_akademy.motus.leo.business.*;
 import java.time.LocalDate;
 import java.util.*;
 
-
 public class App {
+
+    private static ArrayList<Mot> mots = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ajouterMots();
+    }
+
+    private static void ajouterMots() {
+        mots.add(new Mot("piscine"));
+        mots.add(new Mot("changer"));
+        mots.add(new Mot("courant"));
+        mots.add(new Mot("energie"));
+        mots.add(new Mot("fournir"));
+        mots.add(new Mot("jardins"));
+        mots.add(new Mot("lecteur"));
+        mots.add(new Mot("marquer"));
+        mots.add(new Mot("plainte"));
+        mots.add(new Mot("riviere"));
+    }
     
 }
