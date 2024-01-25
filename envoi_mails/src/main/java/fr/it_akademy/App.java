376235a6bb6f38ff94620e92de1343524a3c752b
Leo_m <l.marque69210@gmail.com>
2024-01-25 16:50:00 +0100

    opla

--- envoi_mails/src/main/java/fr/it_akademy/App.java
+++ envoi_mails/src/main/java/fr/it_akademy/App.java
@@ -1,13 +1,31 @@
 package fr.it_akademy;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws EmailException
     {
-        System.out.println( "Hello World!" );
+        try{
+            Email email = new SimpleEmail();
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(465);
+            email.setAuthenticator(new DefaultAuthenticator("l.marque@it-students.fr", "itakademymail"));
+            email.setSSLOnConnect(true);
+            email.setFrom("KimJong-Un@gmail.com");
+            email.setSubject("TestMail");
+            email.setMsg("This is a test mail ... :-)");
+            email.addTo("a.falcoz@it-students.fr");
+            email.send();
+        }catch (Exception e) {
+            System.out.println("Email pas envoyé " + e.getMessage());
+        }
     }
 }
