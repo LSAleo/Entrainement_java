8c0aca0b93eeae5004a8ae254aec3b5dd424a626
LSAleo <l.marque@it-students.fr>
2024-01-29 12:02:47 +0100

    fin envoi mail, début minecraft

--- envoi_mails/src/main/java/fr/it_akademy/App.java
+++ envoi_mails/src/main/java/fr/it_akademy/App.java
@@ -1,9 +1,11 @@
 package fr.it_akademy;
 
 import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailAttachment;
 import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.SimpleEmail;
+import org.apache.commons.mail.MultiPartEmail;
+import org.apache.commons.mail.HtmlEmail;
+
 
 /**
  * Hello world!
@@ -14,16 +16,26 @@ public class App
     public static void main( String[] args ) throws EmailException
     {
         try{
-            Email email = new SimpleEmail();
-            email.setHostName("smtp.googlemail.com");
-            email.setSmtpPort(465);
-            email.setAuthenticator(new DefaultAuthenticator("l.marque@it-students.fr", "itakademymail"));
+            HtmlEmail email = new HtmlEmail();
+            email.setHostName("ssl0.ovh.net");
+            email.setSmtpPort(587);
+            email.setAuthenticator(new DefaultAuthenticator("apprenants.it_akademy@clelia.fr", "P61;Java21"));
             email.setSSLOnConnect(true);
             email.setFrom("KimJong-Un@gmail.com");
-            email.setSubject("TestMail");
-            email.setMsg("This is a test mail ... :-)");
-            email.addTo("a.falcoz@it-students.fr");
+            email.setSubject("Envoi de missiles");
+
+            email.setMsg("<html><body><p>Ceci est un message envoyé par <b>Kim Jong-Un</b> avec du <i>HTML</i>... :-)</p></body></html>");
+
+            EmailAttachment attachment = new EmailAttachment();
+            attachment.setPath("envoi_mails\\src\\main\\java\\fr\\it_akademy\\opla.jpg");
+            attachment.setDisposition(EmailAttachment.ATTACHMENT);
+            attachment.setDescription("Description du fichier");
+            attachment.setName("Le leader suprême"); 
+            email.attach(attachment);
+
+            email.addTo("fxcote@clelia.fr");
             email.send();
+            System.out.println("email envoyé avec succès");
         }catch (Exception e) {
             System.out.println("Email pas envoyé " + e.getMessage());
         }
